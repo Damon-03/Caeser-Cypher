d4395da19ff3327f3a7d69ac33ea77b860802361
Damon-03 <73558661+Damon-03@users.noreply.github.com>
2020-11-12 22:46:38 +1300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaeserCypher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter you message"); 
+            string message = Console.ReadLine(); // Recieves the Message you want to encode.
+            int mesLen = message.Length; // Stores the length of the message in a int.
+            string[] messageChar = new string[mesLen]; // Creates an array with the size of the message.
+            for(int i = 0; i < mesLen; i++) // Stores the characters in the message into the array.
+            {
+                messageChar[i] = message[i];
+            }
+        }
+    }
+}
